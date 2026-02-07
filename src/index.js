34ec61d406609279adab3da99929153cef009d64
iMAN <imankamali75@gmail.com>
2026-02-07 23:15:40 +0330

    chore: add initial library entry point

---
+++ src/index.js
@@ -0,0 +1,7 @@
+export function attachWasmLogger(_Module, _options = {}) {
+  throw new Error("blablalba");
+}
+
+export function autoAttachWasmLogger(_options = {}) {
+  throw new Error("blalabla");
+}
